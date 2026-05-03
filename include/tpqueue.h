5543f1499a67525c9fc851be9878e1c1dee5939e
yaroszub <yaroslav.zubkov.2020@bk.ru>
2026-05-03 18:35:29 +0300

    Зубков Ярослав 25-ИСТ-3

--- include/tpqueue.h
+++ include/tpqueue.h
@@ -2,9 +2,71 @@
 #ifndef INCLUDE_TPQUEUE_H_
 #define INCLUDE_TPQUEUE_H_
 
+#include <stdexcept>
+
 template<typename T>
 class TPQueue {
-  // реализация шаблона очереди с приоритетом на связанном списке
+ private:
+  struct Node {
+    T data;
+    Node* next;
+
+    explicit Node(const T& value) : data(value), next(nullptr) {}
+  };
+
+  Node* head;
+
+ public:
+  TPQueue() : head(nullptr) {}
+
+  ~TPQueue() {
+    clear();
+  }
+
+  TPQueue(const TPQueue&) = delete;
+  TPQueue& operator=(const TPQueue&) = delete;
+
+  bool empty() const {
+    return head == nullptr;
+  }
+
+  void clear() {
+    while (head != nullptr) {
+      Node* tmp = head;
+      head = head->next;
+      delete tmp;
+    }
+  }
+
+  void push(const T& value) {
+    Node* node = new Node(value);
+
+    if (head == nullptr || value.prior > head->data.prior) {
+      node->next = head;
+      head = node;
+      return;
+    }
+
+    Node* cur = head;
+    while (cur->next != nullptr && cur->next->data.prior >= value.prior) {
+      cur = cur->next;
+    }
+
+    node->next = cur->next;
+    cur->next = node;
+  }
+
+  T pop() {
+    if (head == nullptr) {
+      throw std::out_of_range("TPQueue is empty");
+    }
+
+    Node* tmp = head;
+    T result = head->data;
+    head = head->next;
+    delete tmp;
+    return result;
+  }
 };
 
 struct SYM {
